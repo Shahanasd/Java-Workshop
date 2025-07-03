48c5d5700a6582a8cb0df00b33733c99ebf9ad50
Shahanasd <shahananirmal@gmail.com>
2025-07-03 15:10:12 +0530

    Subclassofbuilding

---
+++ src/com/vetias/java/workshop/tempdata/beans/Academicbuilding.java
@@ -0,0 +1,6 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class Academicbuilding extends Building {
+    private boolean isLibraryAvailable;
+    private boolean isAuditoriumAvailable;
+}
---
+++ src/com/vetias/java/workshop/tempdata/beans/Hostel.java
@@ -0,0 +1,6 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class Hostel extends Building{
+     private boolean isMessAvailable;
+     private int numberOfRooms;
+}
