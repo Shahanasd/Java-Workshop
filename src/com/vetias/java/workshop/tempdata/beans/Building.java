1ffa084de01b3c6a5eb76c22ccfda73353702e15
Shahanasd <shahananirmal@gmail.com>
2025-07-03 13:56:27 +0530

    Building

---
+++ src/com/vetias/java/workshop/tempdata/beans/Building.java
@@ -0,0 +1,19 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class Building {
+    private String name;
+    private String area;
+
+    public void setName(String aName) {
+        name = aName;
+    }  
+    public String getName() {
+        return name;
+    }
+    public void setArea(String aArea) {
+        area = aArea;
+    }
+    public String getArea() {
+        return area;
+    }
+}
