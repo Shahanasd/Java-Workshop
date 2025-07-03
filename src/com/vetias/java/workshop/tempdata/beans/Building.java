13210f5582eee65335dde6b013421ab05ab72374
Shahanasd <shahananirmal@gmail.com>
2025-07-03 15:14:23 +0530

    Temperaturedata

--- src/com/vetias/java/workshop/tempdata/beans/Building.java
+++ src/com/vetias/java/workshop/tempdata/beans/Building.java
@@ -1,8 +1,12 @@
 package src.com.vetias.java.workshop.tempdata.beans;
 
+import java.time.LocalDateTime;
+
 public class Building {
     private String name;
     private String area;
+    private LocalDateTime openTime;
+    private LocalDateTime closeTime;
 
     public void setName(String aName) {
         name = aName;
