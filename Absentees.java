b7be01323304b87645211c5cadfa71eabd611f80
Shahanasd <shahananirmal@gmail.com>
2025-07-03 10:26:15 +0530

    absent

--- Absentees.java
+++ Absentees.java
@@ -3,7 +3,6 @@ public class Absentees {
     public static void main(String[] args) {
         String[][] students={{"01","John Doe"},{"02","Jane Smith"},{"03","Alice Johnson"}};
         System.out.println("Students list: "+Arrays.deepToString(students));
-        String absentrollno="02";
         for(int i=0;i<students.length;i++){
             if(students[i][0].equals("02")){
                 System.out.println("Absent student: "+students[i][1]);
