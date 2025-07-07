145cff2f507c417d8229f84165e372ce20c2d3f3
Shahanasd <shahananirmal@gmail.com>
2025-07-07 10:59:20 +0530

    String

---
+++ src/com/vetias/java/workshop/basics/Strings.java
@@ -0,0 +1,11 @@
+package src.com.vetias.java.workshop.basics;
+public class Strings {
+
+    public static void main(String[] args) {
+        String str1="Shahana";
+        String str2="sd";
+        String str3=str1+" "+str2;
+        System.out.println(str3);
+        System.out.println(str3.toUpperCase());
+    }
+}
