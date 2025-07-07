2400e74d4df557723ce71f226347147a5fac3bc1
Shahanasd <shahananirmal@gmail.com>
2025-07-07 09:29:52 +0530

    Hostel

--- src/com/vetias/java/workshop/tempdata/beans/Hostel.java
+++ src/com/vetias/java/workshop/tempdata/beans/Hostel.java
@@ -16,4 +16,29 @@ public class Hostel extends Building{
      public void setNumberOfRooms(int numberOfRooms) {
          this.numberOfRooms = numberOfRooms;
      }
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Hostel Details:");
+        System.out.println("Mess Available: " + isMessAvailable);
+        System.out.println("Number of Rooms: " + numberOfRooms);
+    }
+    @Override
+    public String toString() {
+        return "Hostel{" +
+                "isMessAvailable=" + isMessAvailable +
+                ", numberOfRooms=" + numberOfRooms +
+                "} " + super.toString();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Hostel)) return false;
+        if (!super.equals(o)) return false;
+
+        Hostel hostel = (Hostel) o;
+
+        if (isMessAvailable != hostel.isMessAvailable) return false;
+        return numberOfRooms == hostel.numberOfRooms;
+    }
 }
