2aa6d2c7f697a5096998e33571b042552e04be6f
Shahanasd <shahananirmal@gmail.com>
2025-07-07 09:36:53 +0530

    Zone

--- src/com/vetias/java/workshop/tempdata/beans/Zone.java
+++ src/com/vetias/java/workshop/tempdata/beans/Zone.java
@@ -30,4 +30,33 @@ public class Zone {
     public String getArea() {
         return area;
     }
+    @Override
+    public String toString() {
+        return "Zone{" +
+                "name='" + name + '\'' +
+                ", zoneId='" + zoneId + '\'' +
+                ", Type='" + Type + '\'' +
+                ", area='" + area + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Zone)) return false;
+
+        Zone zone = (Zone) o;
+
+        if (!name.equals(zone.name)) return false;
+        if (!zoneId.equals(zone.zoneId)) return false;
+        if (!Type.equals(zone.Type)) return false;
+        return area.equals(zone.area);
+    }
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + zoneId.hashCode();
+        result = 31 * result + Type.hashCode();
+        result = 31 * result + area.hashCode();
+        return result;
+    }
 }
