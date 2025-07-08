c6ac80ff9204c03f9b7388d030cccbbb8219c306
Shahanasd <shahananirmal@gmail.com>
2025-07-08 09:55:52 +0530

    Datetime

---
+++ src/com/vetias/java/workshop/basics/Datetime.java
@@ -0,0 +1,14 @@
+package src.com.vetias.java.workshop.basics; 
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Datetime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year of birth: ");
+        int yearofbirth=scanner.nextInt();
+        int currentyear=LocalDate.now().getYear();
+        int age=currentyear-yearofbirth;
+        System.out.println(age);
+    }
+}
