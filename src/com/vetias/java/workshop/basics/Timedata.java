0b7c969f42c708243e73aa16108dd3c00cf0cb1e
Shahanasd <shahananirmal@gmail.com>
2025-07-08 10:57:06 +0530

    Timedate

---
+++ src/com/vetias/java/workshop/basics/Timedata.java
@@ -0,0 +1,14 @@
+package src.com.vetias.java.workshop.basics;
+
+import java.time.LocalTime;
+
+public class Timedata {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        LocalTime registrationTime=LocalTime.of(18, 59,59);
+        int hoursleft= registrationTime.getHour() - currentTime.getHour();
+        int minutesleft=registrationTime.getMinute()-currentTime.getMinute();
+        int secondsleft=registrationTime.getSecond()-currentTime.getSecond();
+        System.out.println("You have "+hoursleft+" hours, "+minutesleft+" minutes, and "+secondsleft+" seconds left to register.");
+    }
+}
