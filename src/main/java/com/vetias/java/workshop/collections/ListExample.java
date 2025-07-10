4c403faeefd8c4d9a3d4963112ae076cda44fcda
Shahanasd <shahananirmal@gmail.com>
2025-07-10 14:54:14 +0530

    ListExample

--- src/main/java/com/vetias/java/workshop/collections/ListExample.java
+++ src/main/java/com/vetias/java/workshop/collections/ListExample.java
@@ -1,6 +1,7 @@
 package com.vetias.java.workshop.collections;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ListExample {
@@ -11,8 +12,14 @@ public class ListExample {
         names.add("Vinu");
         
         System.out.println(names);
+        names.set(2, "ajeeee");
         for(int i=0;i<names.size();i++){
             System.out.println("Name : "+names.get(i));
         }
+        names.sort(Comparator.naturalOrder());
+        System.out.println("Sorted names: " + names);
+        names.sort(Comparator.reverseOrder());
+        System.out.println("Reverse sorted names: " + names);
+        
     }
 }
