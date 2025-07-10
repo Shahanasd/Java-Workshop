82dbf6fbef89f7ec62c19bd835cf2a7fc9148f09
Shahanasd <shahananirmal@gmail.com>
2025-07-10 13:28:15 +0530

    Maven program

--- src/test/java/com/vetias/java/workshop/basics/string/WordCount.java
+++
@@ -1,8 +0,0 @@
-package com.vetias.java.workshop.basics.string;
-
-public class WordCount {
-    public int count(String inputString)
-    {
-        return 0;
-    }
-}
--- src/test/java/com/vetias/java/workshop/basics/string/WordCounttest.java
+++ src/test/java/com/vetias/java/workshop/basics/string/WordCounttest.java
@@ -1,6 +1,9 @@
 package com.vetias.java.workshop.basics.string;
 
 import org.junit.jupiter.api.Test;
+
+import com.vetias.java.workshop.basics.WordCount;
+
 import org.junit.jupiter.api.Assertions;
 
 public class WordCounttest {
@@ -8,6 +11,13 @@ public class WordCounttest {
     public void testWordCount(){
         WordCount wordcount= new WordCount();
         int count = wordcount.count("This is a java program to test the word count functionality in java");
-        Assertions.assertEquals(12,count);
+        Assertions.assertEquals(13,count);
+    }
+
+    @Test
+    public void testAnotherString(){
+        WordCount wordcount= new WordCount();
+        int count = wordcount.count("This is another test string");
+        Assertions.assertEquals(5,count);
     }
 }
