6c3770feddaebc29db3a336b376aedea3467a24b
Shahanasd <shahananirmal@gmail.com>
2025-07-11 10:56:26 +0530

    List Example 2

---
+++ src/main/java/com/vetias/java/workshop/collections/Listeg.java
@@ -0,0 +1,20 @@
+package com.vetias.java.workshop.collections;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Listeg {
+    
+
+
+
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Shahana");
+        names.add("Madhuja");
+        names.add("Vinu");
+        names.forEach(System.out::println);
+    }
+}
